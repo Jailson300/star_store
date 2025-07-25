6f4a9e35f406f05108303e1fe6eb0bf8935c498f
WinterSunset95 <wintersunset95@gmail.com>
2025-07-25 05:45:54 +0530

    Initial test of confirmation feature

--- functions/test/index.test.ts
+++ functions/test/index.test.ts
@@ -8,11 +8,55 @@
 //  measurementId: "G-T0SEWLNBV4"
 //}, '/home/autumn/Downloads/winter-f3cb5-7a8c82e25a10.json')
 
-import "mocha"
-import { expect } from "chai"
-import * as sinon from "sinon"
-import * as fft from "firebase-functions-test"
+import { describe, it, expect, beforeAll, afterAll, vi } from "vitest"
+import fft from "firebase-functions-test"
+import admin from "firebase-admin"
+import axios from "axios"
 
 import { createStarStoreOrder, sendOrderNotification } from "../src/index"
+import { CallableRequest } from "firebase-functions/https"
 
 const testEnv = fft()
+
+describe("Cloud Functions: Callable", () => {
+	vi.mock('axios')
+
+	beforeAll(() => {
+		vi.spyOn(admin, "initializeApp").mockImplementation(() => {})
+	})
+
+	afterAll(() => {
+		vi.restoreAllMocks()
+		testEnv.cleanup()
+	})
+
+
+	it("Should send order notification", async () => {
+		console.log("Hello")
+		const testData = {
+			razorpay_order_id: "order_Qx1zCJ2DqUd4re",
+			razorpay_payment_id: "pay_Qx1zO05Vlw4sZ0",
+			razorpay_signature: "76b7cfee6ebab1b6d7eee397347d92e40d529508e397e1af3dfb836f757bcc65",
+			id: "454855763",
+			name: "ᖇᗩᎥᑎ",
+			package: "Diamond 257",
+			server: "2321",
+			cost: "Rs. 330"
+		}
+
+		const callableReq: CallableRequest<any> = {
+			data: testData
+		}
+
+		vi.mocked(axios.post).mockResolvedValue({ status: 200, data: { ok: true } });
+
+		const wrapped = testEnv.wrap(sendOrderNotification)
+		const result = await wrapped(callableReq)
+
+		console.log(result)
+		expect(result).to.be.an("object")
+		expect(result?.success).to.equal(true)
+		expect(axios.post).toHaveBeenCalledOnce();
+
+	})
+})
