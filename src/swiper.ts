13684ae561b59cee0852e67cae82bc682eae943b
boinaohemam <boinaohemam@gmail.com>
2025-08-13 23:40:41 +0530

    removal of ads.js  and cleaning code 13/8/25

--- src/swiper.ts
+++ src/swiper.ts
@@ -14,31 +14,4 @@ const swiper = new Swiper('.swiper', {
 	slidesPerView: 1,
 });
 
-// Turns out we don't need this
-// We can use the same swiper instance for all swiper slides
-// in the project
-/*
-const swiper2 = new Swiper('.swiper2', {
-	wrapperClass: 'swiper-wrapper2',
-	direction: 'horizontal',
-	loop: true,
-	loopAddBlankSlides: true,
 
-	autoplay: {
-		delay: 5000,
-		disableOnInteraction: false,
-	},
-
-	slidesPerView: 2,
-	spaceBetween: 20,
-	centeredSlides: true,
-	breakpoints: {
-		640: {
-			slidesPerView: 4,
-		},
-		1880: {
-			slidesPerView: 6,
-		},
-	}
-});
-*/
